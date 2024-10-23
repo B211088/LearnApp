c00c56e32e080e78233b90bdd70012ae95dc0b3c
B211088 <139199932+B211088@users.noreply.github.com>
2024-10-23 23:15:38 +0700

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const User = require("../models/User");
+const User = require("../models/user");
 const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
 const verifyToken = require("../middleware/auth");
